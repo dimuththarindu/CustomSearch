d06740d64bc9cab413cbee2549b3eb6fb571951f
DT <dimuththarindu@live.com>
2018-08-09 21:41:23 +0530

    Updates
    New themes
    New functions

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+    var mystring = document.getElementById("myInput").value + " -inurl:(htm|html|php|pls|txt) intitle:index.of (3gp|avi|flv|mkv|mov|mp4|mpeg|mpg|wmv)";
+    mystring = encodeURI(mystring);
+    mystring ="https://www.google.com/search?q=" + mystring;
+    //document.getElementById("SearchURLOutput").innerHTML = mystring;
+    //window.location.href = mystring;
+    window.open(mystring, '_blank');
+}
