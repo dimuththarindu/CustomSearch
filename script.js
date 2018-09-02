81d795150c970822901da55fc2791a017771d168
Dimuth Tharindu <dimuththarindu@live.com>
2018-09-03 03:30:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    var mystring = document.getElementById("myInput").value + " -inurl:(htm|html|php|pls|txt) intitle:index.of “last modified” (3gp|avi|flv|mkv|mov|mp4|mpeg|mpg|wmv)";
+    var mystring = document.getElementById("myInput").value + " -inurl:(htm|html|php|pls|txt) intitle:index.of (3gp|avi|flv|mkv|mov|mp4|mpeg|mpg|wmv)";
     mystring = encodeURI(mystring);
     mystring ="https://www.google.com/search?q=" + mystring;
     //document.getElementById("SearchURLOutput").innerHTML = mystring;
