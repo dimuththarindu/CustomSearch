37d80614cbbafcf985e9b4a8afa2124a8826473a
DT <dimuththarindu@live.com>
2018-09-16 22:30:59 +0530

    Fix bugs

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function myFunction() {
+function funWebIndex() {
     var userInput = document.getElementById("myInput").value;
     var webIndexString = userInput + " -inurl:(htm|html|php|pls|txt) intitle:index.of (3gp|avi|flv|mkv|mov|mp4|mpeg|mpg|wmv)";
-    webIndexString = encodeURI(userInput);
-    webIndexString ="https://www.google.com/search?q=" + webIndexString;
-
-    var googleDriveString = "site:docs.google.com/document/d";
-    googleDriveString = userInput + googleDriveString;
-    
+    webIndexString = encodeURI(webIndexString);
+    webIndexString ="https://www.google.com/search?q=" + webIndexString;    
     window.open(webIndexstring, '_blank');
+}
+
+function funGoogleDrive() {
+    var userInput = document.getElementById("myInput").value;
+    var googleDriveString = userInput + " site:docs.google.com/document/d";
+    googleDriveString = encodeURI(googleDriveString);
+    googleDriveString ="https://www.google.com/search?q=" + googleDriveString;
     window.open(googleDriveString, '_blank');    
-    return false;
 }
