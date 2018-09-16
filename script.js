4f87a5f97ee6d1426a745c5563a45d8e3d45eb25
DT <dimuththarindu@live.com>
2018-09-16 22:05:04 +0530

    Fix minor error

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 function myFunction() {
     var userInput = document.getElementById("myInput").value;
     var webIndexString = userInput + " -inurl:(htm|html|php|pls|txt) intitle:index.of (3gp|avi|flv|mkv|mov|mp4|mpeg|mpg|wmv)";
-    webIndexString = encodeURI(mystring);
+    webIndexString = encodeURI(userInput);
     webIndexString ="https://www.google.com/search?q=" + webIndexString;
 
     var googleDriveString = "site:docs.google.com/document/d";
     googleDriveString = userInput + googleDriveString;
-    //document.getElementById("SearchURLOutput").innerHTML = mystring;
-    //window.location.href = mystring;
     
-    window.open(googleDriveString, '_blank');
     window.open(webIndexstring, '_blank');
+    window.open(googleDriveString, '_blank');    
     
 }
