485800cfe916ceb1eb4b9fca0ae2a8f93f3ba964
DT <dimuththarindu@live.com>
2018-09-16 23:03:14 +0530

    Fix Bugs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function funWebIndex() {
-    alert("this function is called");
+    //alert("this function is called");
     var userInput = document.getElementById("myInput").value;
     var webIndexString = userInput + " -inurl:(htm|html|php|pls|txt) intitle:index.of (3gp|avi|flv|mkv|mov|mp4|mpeg|mpg|wmv)";
     webIndexString = encodeURI(webIndexString);
@@ -8,7 +8,7 @@ function funWebIndex() {
 }
 
 function funGoogleDrive() {
-    alert("this function is called");
+    //alert("this function is called");
     var userInput = document.getElementById("myInput").value;
     var googleDriveString = userInput + " site:docs.google.com/document/d";
     googleDriveString = encodeURI(googleDriveString);
