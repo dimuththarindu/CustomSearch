fb1346e2a2caa5232e5730d030949dd2c3fa874e
DT <dimuththarindu@live.com>
2018-09-16 21:56:42 +0530

    Added Google Drive Search

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function myFunction() {
-    var mystring = document.getElementById("myInput").value + " -inurl:(htm|html|php|pls|txt) intitle:index.of (3gp|avi|flv|mkv|mov|mp4|mpeg|mpg|wmv)";
-    mystring = encodeURI(mystring);
-    mystring ="https://www.google.com/search?q=" + mystring;
+    var userInput = document.getElementById("myInput").value;
+    var webIndexString = userInput + " -inurl:(htm|html|php|pls|txt) intitle:index.of (3gp|avi|flv|mkv|mov|mp4|mpeg|mpg|wmv)";
+    webIndexString = encodeURI(mystring);
+    webIndexString ="https://www.google.com/search?q=" + webIndexString;
+
+    var googleDriveString = "site:docs.google.com/document/d";
+    googleDriveString = userInput + googleDriveString;
     //document.getElementById("SearchURLOutput").innerHTML = mystring;
     //window.location.href = mystring;
-    window.open(mystring, '_blank');
+    //
+    window.open(webIndexstring, '_blank');
+    window.open(googleDriveString, '_blank');
 }
